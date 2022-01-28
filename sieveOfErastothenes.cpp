f524510c0e45f04f07c10987677705bba4d5ac3f
Akhilendra <47774234+Akhilendra01@users.noreply.github.com>
2022-01-28 22:29:29 +0530

    Add files via upload

---
+++ sieveOfErastothenes.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n";
+typedef long long ll;
+
+
+void sieve(ll n, vector<bool> &isPrime){
+    isPrime[0]=false; 
+    isPrime[1]=false;
+    for(ll i=2;i<=n;i++){
+        if(isPrime[i]){
+            for(int j=i*i;j<=n;j+=i){
+                isPrime[j]=false;
+            }
+        }
+    }
+}
+
+int main(int argc, char const *argv[]){
+
+    ll n; cin>>n;
+    vector<bool> isPrime(n, true) ; 
+    sieve(n, isPrime);
+    return 0;
+}
