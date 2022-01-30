af3d1f3fe6ada56029a08cfe32bee549778fa6a6
Akhilendra <47774234+Akhilendra01@users.noreply.github.com>
2022-01-30 14:47:19 +0530

    Add files via upload

---
+++ modularExponentiation.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n";
+typedef long long ll;
+
+ll modularExponentiation(ll base, ll power, ll p){
+    ll res=1;
+    while(power){
+        if(power%2){
+            res=(res*base)%p;
+            power --;
+        }
+        base=(base*base)%p;
+        power/=2;
+    }
+    return res;
+}
+
+int main(int argc, char const *argv[]){
+
+    ll x, y, p; cin>>x>>y>>p;
+    cout<<modularExponentiation(x, y, p);
+    return 0;
+}
